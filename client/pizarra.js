0cce524563d2b89ed2d3bed96b347e0e39dc8a58
Emilio <emilioplatzer@gmail.com>
2016-03-13 23:50:55 -0300

    Pongo el fondo

---
+++ client/pizarra.js
@@ -0,0 +1,17 @@
+function Pizarra(){
+}
+
+var pizarra = new Pizarra();
+
+function resizeNow(){
+    lateral.style.height = window.innerHeight - superior.clientHeight -2 + 'px';
+    central.style.height = window.innerHeight - superior.clientHeight -2 + 'px';
+}
+
+window.addEventListener('load', function(){
+    resizeNow();
+});
+
+window.addEventListener('resize', resizeNow);
+
+central.addEventListener('click', pizarra.doClick);
--- install/create_db.sql
+++ install/create_db.sql
@@ -9,10 +9,21 @@ grant all on schema piz to pizarra_user;
 create table piz.users(
   username text primary key,
   md5pass text,
+  kind text,
   rol text
 );
 alter table piz."users" owner to pizarra_user;
 
 insert into piz.users 
-  select 'test'||n, md5('clave'||n||'test'||n), 'test'
+  select 'test'||n, md5('clave'||n||'test'||n), 'direct', 'test'
     from generate_series(1,10) n;
+
+create table piz.pizarras(
+  pizarra text primary key,
+  username text references piz.users(username),
+  content text,
+  constraint "pizarra must be the username" check (pizarra=username)
+);
+alter table piz.pizarras owner to pizarra_user;
+
+insert into piz.pizarras (pizarra, username, content) select username, username, '{"version": "0.0.1"}' from piz."users";
